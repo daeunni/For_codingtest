307ba1c2c31040e50c051942dbbeb33fc1c7ea31
Daeun Lee <62705839+daeunni@users.noreply.github.com>
2022-01-06 13:45:26 +0900

    Update 거스름돈.py

--- Greedy/거스름돈.py
+++ Greedy/거스름돈.py
@@ -1,5 +1,7 @@
 
-# coin : 500, 100, 50, 10 won > minimum count
+"""
+최소 개수의 동전으로 N원을 만들기 
+"""
 
 N = 1260
 wallet = [500, 100, 50, 10]
@@ -7,8 +9,8 @@ wallet = [500, 100, 50, 10]
 def change_money(N, wallet):
     count = 0
     for coin in wallet:
-        count += N // coin
-        N %= coin
+        count += N // coin     # 몫만큼 count에 더하고 
+        N %= coin          # 나머지를 남기기 
 
     print(count)
 
