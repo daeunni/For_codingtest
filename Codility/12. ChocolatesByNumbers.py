e53f512e35476faa25bf85412347f70001678fb6
Daeun Lee <62705839+daeunni@users.noreply.github.com>
2022-03-08 14:39:09 +0900

    Create 12. ChocolatesByNumbers.py

---
+++ Codility/12. ChocolatesByNumbers.py
@@ -0,0 +1,14 @@
+# 최대공약수의 배수를 구하는 문제 
+
+def solution(N, M):
+    
+    a, b = N, M
+    
+    # 최대공약수(gcd)를 구하는 유클리드호제법 이용 
+    while (b):
+        a, b = b, a % b    # 계속 나머지를 구하다보면 언젠간 0이 나오는 시점이 있음. 그때 멈추기 
+    
+    # 최대공약수 
+    gcd = a
+    answer = N // gcd      # 최대공약수의 배수 구하기 
+    return answer 
